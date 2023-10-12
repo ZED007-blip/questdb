37acf8f5281785fa98ca62a0e0e11bc684b217ea
ZED007-blip <147626533+ZED007-blip@users.noreply.github.com>
2023-10-12 14:57:53 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+emptylist = []  
+mylist = [20, 20, 'H', "Hello"]  
+nestedlist = [[1, 2], mylist]  
+print("Created lists:")  
+print("emptylist:", emptylist)  
+print("mylist:", mylist)  
+print("nestedlist:", nestedlist)  
+print("Concatenating mylist and nestedlist:", mylist + nestedlist)  
+print("Repeating the elements of a list:", mylist*3)  
